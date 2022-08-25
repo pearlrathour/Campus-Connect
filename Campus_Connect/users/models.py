41a091d0b7bcaff3d44242dda443eee6d4620fc3
ojas-2003 <21bec052@nith.ac.in>
2022-08-25 22:34:06 +0530

    Made dynamic

--- Campus_Connect/users/models.py
+++ Campus_Connect/users/models.py
@@ -21,6 +21,9 @@ class profile(models.Model):
 
 
 class blog(models.Model):
-    user=models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    user=models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    tags=models.ForeignKey('tags', blank=True, null=True , on_delete=models.CASCADE)
     title=models.CharField(max_length=200, null=True, blank=True)
     all=models.TextField(blank=True, null=True)
+    def __str__(self):
+        return self.title
--- Campus_Connect/users/views.py
+++ Campus_Connect/users/views.py
@@ -3,20 +3,27 @@ from xml.sax import make_parser
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from . models import profile
+from . models import profile, tags, blog
 from django.contrib.auth.models import User
+
 
 
 def prof(request):
 #     users=User.objects.get(id=1)
     profiles=profile.objects.get(id=7)
     
+    blogs=blog.objects.all()
+    tagss=tags.objects.all()
     context={
-        "name" : profiles.Name,
-    }
+                "tag":tagss,
+                "title":blogs,
+                "name" : profiles.Name,
+                "blog":blogs
+        }
     return render(request, "club.html",context)
 
-def blog(request):
-        return render(request, "blog.html")
+# def blogs(request):
+        
+#         return render(request, "blog.html", context)
 
 
