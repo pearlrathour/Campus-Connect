914bb2dfa06e0ef41a438813d6a93fcff9159b1e
ojas-2003 <21bec052@nith.ac.in>
2022-08-26 07:49:31 +0530

    Added societies

--- Campus_Connect/Basics/views.py
+++ Campus_Connect/Basics/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from Users.models import profile
+from django.contrib.auth.models import User
+from django.contrib import messages
 
-# Create your views here.
+
+def home(request):
+    return render(request, "landing.html")
+
+def login_page(request):
+    if request.method=='POST':
+        loginusername=request.POST['loginusername']
+        loginpassword=request.POST['loginpassword']
+        user=authenticate(request, username=loginusername, password=loginpassword)
+        if user is not None:
+            login(request, user)
+            # messages.success(request, "success")
+            return redirect('home')
+        else:
+            
+            messages.error(request, "error")
+            
+    return render(request, "login.html")
